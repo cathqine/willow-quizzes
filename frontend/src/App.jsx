0386e5e9c836d437107bbfbe5dd7cf68ab092210
Catherine <z5359881@student.unsw.edu.au>
2024-05-14 22:39:30 +1000

    added changed App files: css and jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home'
+import Dashboard from './components/pages/Dashboard'
+import SampleQuiz from './components/sample-quiz/SampleQuiz'
+import SampleQuizResults from './components/sample-quiz/SampleQuizResults';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element=<Home /> />
+        <Route path="/quizzes" element=<Dashboard /> />
+        <Route path="/sample-quiz" element=<SampleQuiz /> />
+        <Route path="/results-sample-quiz" element=<SampleQuizResults /> />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
